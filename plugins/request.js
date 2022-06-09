f7d761a7fde9c080808f1ad3d8a7ec028935f06a
lijiaxing <1654083539@qq.com>
2022-06-10 01:32:11 +0800

    update api

--- plugins/request.js
+++ plugins/request.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 
 // 创建请求对象
 export const request = axios.create({
-  baseURL: 'http://realworld.api.fed.lagounews.com',
+  baseURL: 'https://api.realworld.io',
   // baseURL: 'https://conduit.productionready.io'
 });
 
